aecaa0bbb58b92b4a0c2e9217e94b4974668de27
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2022-03-10 13:19:37 -0600

    Livewire basics episode 2, form component example.

---
+++ app/Http/Livewire/ContactForm.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Mail\ContactFormMailable;
+use Illuminate\Support\Facades\Mail;
+use Livewire\Component;
+
+class ContactForm extends Component
+{
+
+    public $name;
+    public $email;
+    public $phone;
+    public $message;
+    public $successMessage;
+
+    protected $rules = [
+        'name' => 'required',
+        'email' => 'required|email',
+        'phone' => 'required|min:10',
+        'message' => 'required|min:5',
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submitForm()
+    {
+        $contact = $this->validate();
+
+        $contact['name'] = $this->name;
+        $contact['email'] = $this->email;
+        $contact['phone'] = $this->phone;
+        $contact['message'] = $this->message;
+
+        sleep(1);
+    
+        Mail::to('andre@andre.com')->send(new ContactFormMailable($contact));
+
+        $this->successMessage = 'We received your message successfully and will get back to you shortly';
+        
+        $this->resetForm();
+    
+        // return back()->with('success_message', 'We received your message successfully and will get back to you shortly!');
+    }
+
+    private function resetForm()
+    {
+        $this->name = '';
+        $this->email = '';
+        $this->phone = '';
+        $this->message = '';
+    }
+
+    public function render()
+    {
+        return view('livewire.contact-form');
+    }
+}
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-blue-500">
+    <span> {{ $count }}</span>
+    <button wire:click="decrement">-</button>
+    <button wire:click="increment">+</button>
+</div>
